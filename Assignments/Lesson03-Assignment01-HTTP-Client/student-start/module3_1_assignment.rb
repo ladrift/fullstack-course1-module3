5cf6fc5df40163df9362528f93b8170e9c067c5c
Ziyi Yan <cxfyzy@gmail.com>
2016-05-31 19:25:39 +0800

    Completed assignment 01 - HTTP client

--- Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
+++ Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
@@ -1,5 +1,18 @@
 require 'httparty'
 
 class Recipe
+  include HTTParty
 
+  base_uri 'http://food2fork.com/api'
+  default_params key: ENV['FOOD2FORK_KEY']
+  format :json
+
+  debug_output $stdout
+
+  def self.for(search_term)
+    response = get('/search', :query => { q: search_term })
+    if response.code == 200
+      response['recipes']
+    end
+  end
 end
